e502d453615846e8c12834fc8c8a7d9250a35e26
GiuseppeVartuli <giuseppevartuli89@gmail.com>
2024-01-05 17:55:29 +0100

    feat tags color and fix img

--- JS/script.js
+++ JS/script.js
@@ -11,19 +11,19 @@ const news = [
     id: "1",
     title: "Scoperta di una nuova specie di papera di gomma",
     content: "Scoperta di una nuova specie di papera di gomma.",
-    tags: ["geo , tech"],
+    tags: ["geo", "tech"],
     author: "Diana Rossi",
     published: "2023-02-11",
-    img: "img/deep-sea.jpg",
+    img: "img/rubber-duck.jpg",
   },
   {
     id: "2",
     title: "Esplorando le profondità marine: il mistero degli abissi",
     content: "Esplorando le profondità marine: il mistero degli abissi",
-    tags: ["viaggi, geo"],
+    tags: ["viaggi", "geo"],
     author: "Fabio Mari",
     published: "2023-03-14",
-    img: "img/kitchen-food.jpg",
+    img: "img/deep-sea.jpg",
   },
   {
     id: "3",
@@ -33,19 +33,28 @@ const news = [
     tags: ["cucina"],
     author: "Marta Bianchi",
     published: "2023-04-20",
-    img: "img/modern-art.jpg",
+    img: "img/kitchen-food.jpg",
   },
   {
     id: "4",
     title: "Arte moderna: oltre i confini convenzionali",
     content:
       "Un'analisi delle tendenze e delle sfide nell'arte contemporanea, con interviste a artisti emergenti.",
-    tags: ["arte, tech"],
+    tags: ["arte", "tech"],
     author: "Gabriele Neri",
     published: "2023-05-29",
-    img: "img/rubber-duck.jpg",
+    img: "img/modern-art.jpg",
   },
 ];
+
+const tagColors = {
+  geo: "green",
+  tech: "darkblue",
+  viaggi: "orange",
+  cucina: "rebeccapurple",
+  arte: "rgb(180, 180, 4)",
+  politica: "grey",
+};
 console.log(news);
 /*
 Step 2 - Stampa dei dati in pagina
@@ -62,31 +71,34 @@ articlesEl.innerHTML = "";
 for (let i = 0; i < news.length; i++) {
   const article = news[i];
   console.log(article);
-  {
-    const cardMarkup = `
-      <div class="cards">
-        <div class="top_card">
-          <h2>${article.title}</h2>
-          <button class="bookmark_empty">
-            <i class="fa-regular fa-bookmark"></i>
-          </button>
-          <button class="bookmark_full">
-            <i class="fa-solid fa-bookmark bookmark_full"></i>
-          </button>
-          </div>
-          <h4>pubblicato da ${article.author}</h4>
-          <p>in data ${article.published}</p>
-          <p id="text">
-            ${article.content}
-          </p>
-          <img src="${article.img}" alt="Papera_di_gomma" />
-            ${article.tags
-              .map((tag) => `<div class="tags">${tag}</div>`)
-              .join("")}
-        </div>
-      </div>
-      `;
 
-    articlesEl.insertAdjacentHTML("beforeend", cardMarkup);
-  }
+  const cardMarkup = `
+    <div class="cards">
+      <div class="top_card">
+        <h2>${article.title}</h2>
+        <button class="bookmark_empty">
+          <i class="fa-regular fa-bookmark"></i>
+        </button>
+        <button class="bookmark_full">
+          <i class="fa-solid fa-bookmark bookmark_full"></i>
+        </button>
+      </div>
+      <h4>pubblicato da ${article.author}</h4>
+      <p>in data ${article.published}</p>
+      <p id="text">
+        ${article.content}
+      </p>
+      <img src="${article.img}" alt="Papera_di_gomma" />
+      <div class="container_tags">
+        ${article.tags
+          .map(
+            (tag) =>
+              `<div class="tags" style="background-color: ${tagColors[tag]}">${tag}</div>`
+          )
+          .join("")} 
+      </div>
+    </div>
+  `;
+
+  articlesEl.insertAdjacentHTML("beforeend", cardMarkup);
 }
